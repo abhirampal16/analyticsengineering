262a681f1fcc65f71495d0c515ed419bd5e972a5
Abhi Rampal <abhirampal17@gmail.com>
2024-06-03 06:47:30 +0000

    lineage

--- models/customers.sql
+++ models/customers.sql
@@ -1,3 +1,5 @@
+
+
 with customers as (
 
     select
@@ -5,7 +7,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from {{ref('stg_customers')}}
 
 ),
 
@@ -17,7 +19,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from {{ref('stg_orders')}}
 
 ),
 
